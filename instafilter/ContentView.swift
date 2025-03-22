19c497b22bed3728ac10152c60591186a9957873
xHadi3 <hadiessam9@gmail.com>
2025-03-22 16:47:29 +0300

    Initial Commit

---
+++ instafilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  instafilter
+//
+//  Created by Hadi Al zayer on 22/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ instafilter/instafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  instafilterApp.swift
+//  instafilter
+//
+//  Created by Hadi Al zayer on 22/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct instafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

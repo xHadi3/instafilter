268080cea987bf1a772f84d66a58c62a477f54dd
xHadi3 <hadiessam9@gmail.com>
2025-03-25 17:57:37 +0300

    adding a load imaga function

--- instafilter/ContentView.swift
+++ instafilter/ContentView.swift
@@ -4,24 +4,33 @@
 //
 //  Created by Hadi Al zayer on 22/09/1446 AH.
 //
-
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var selectedItem : PhotosPickerItem?
+    
     var body: some View {
         NavigationStack{
             VStack{
                Spacer()
-                
-                if let processedImage{
-                    processedImage
-                        .resizable()
-                        .scaledToFill()
-                } else {
-                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                PhotosPicker(selection:$selectedItem){
+                    if let processedImage{
+                        processedImage
+                            .resizable()
+                            .scaledToFill()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
                 }
+                .onChange(of: selectedItem , loadImage)
+                
+                /// to remove the default color
+                //  .buttonStyle(.plain)
                 
                 HStack{
                     Text("Intensity")
@@ -41,6 +50,15 @@ struct ContentView: View {
     
     func changeFilter(){
         
+    }
+    
+    func loadImage(){
+        Task{
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else{ return }
+            
+            guard let inputImage = UIImage(data:imageData) else {return }
+            
+        }
     }
     
 }
